d6fc63f90eec0eac04b69279856b3c60434b0697
Turgut <turqut.a.314@gmail.com>
2020-01-05 23:43:36 +0400

    docs/tests

--- tests/testthat/test-resnet.R
+++ tests/testthat/test-resnet.R
@@ -7,6 +7,8 @@ cifar <- dataset_cifar10()
 
 hypermodel = kerastuneR::HyperResNet(input_shape = list(300L, 300L, 3L), classes = 10L)
 
+testthat::expect_match(hypermodel %>% capture.output(),'kerastuner.applications.resnet.HyperResNet')
+
 tuner = kerastuneR::Hyperband(
   hypermodel = hypermodel,
   objective = 'accuracy',
@@ -14,6 +16,8 @@ tuner = kerastuneR::Hyperband(
   max_epochs = 1,
   directory = 'my_dir',
   project_name='helloworld')
+
+testthat::expect_match(tuner %>% capture.output(),'kerastuner.tuners.hyperband.Hyperband')
 
 train_data = cifar$train$x[1:30,1:32,1:32,1:3]
 test_data = cifar$train$y[1:30,1] %>% as.matrix()
