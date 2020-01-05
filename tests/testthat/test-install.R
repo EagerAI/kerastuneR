e715b3836ec8a7edd7211d535b118663093f01e7
Turgut <turqut.a.314@gmail.com>
2020-01-05 17:29:59 +0400

    test - expect warning (for now)

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -1,8 +1,4 @@
 context("install_kerastuner")
-
-library(keras)
-library(tensorflow)
-library(dplyr)
 library(kerastuneR)
 
 os = switch(Sys.info()[['sysname']],
@@ -10,9 +6,5 @@ os = switch(Sys.info()[['sysname']],
             Linux  = {paste("lin")},
             Darwin = {paste("mac")})
 
-if(os %in% 'lin' | os %in% 'mac') {
-  expect_vector(kerastuneR::install_kerastuner(python_path = '/usr/bin/python3.5'))
-} else {
-  testthat::expect_warning(kerastuneR::install_kerastuner())
-}
+testthat::expect_warning(kerastuneR::install_kerastuner())
 
