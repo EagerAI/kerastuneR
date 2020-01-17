3344154a2342d68f2d9a5c4439036a5333a4f0d9
Turgut <38523699+henry090@users.noreply.github.com>
2020-01-17 17:49:17 +0400

    remove invisible

--- R/reexports.R
+++ R/reexports.R
@@ -1,14 +1,13 @@
 #' @importFrom reticulate use_python
 #' @export
 reticulate::use_python
-NULL
+
 
 #' @importFrom tensorflow tf
 #' @export
 tensorflow::tf
-NULL
+
 
 #' @importFrom reticulate PyClass
 #' @export
 reticulate::PyClass
-NULL
