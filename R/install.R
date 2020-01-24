4c4f3dff4b791eb1139e7cb351d9cc36cb3f7efe
Turgut A <turqut.a.314@gmail.com>
2020-01-24 15:46:14 +0400

    Update install.R

--- R/install.R
+++ R/install.R
@@ -3,6 +3,7 @@
 #' @description This function is used to install the Keras Tuner python module
 
 #' @param version for specific version of Keras tuneR, e.g. "0.9.1"
+#' @param ... other arguments passed to [reticulate::py_install()].
 #' @param restart_session Restart R session after installing (note this will only occur within RStudio).
 #' @export
 install_kerastuner <- function(version = NULL, ..., restart_session = TRUE) {
