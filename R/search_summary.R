28acd0ad005977b3b6dbeed5c46bae18d4690063
Turgut <turqut.a.314@gmail.com>
2020-01-31 09:20:09 +0400

    remove unnecessary parts

--- R/search_summary.R
+++ R/search_summary.R
@@ -4,11 +4,9 @@
 #' @param tuner Requires a tuner object
 #' @export
 search_summary = function(tuner = NULL) {
-  if (is.null(tuner)) {
-    warning('Tuner object does not exist')
-  } else {
+
     tuner$search_space_summary()
-  }
+
 }
 
 #' Print a summary of the search results/best models
@@ -16,10 +14,8 @@ search_summary = function(tuner = NULL) {
 #' @param tuner Requires a tuner object
 #' @param num_trials Shows the top best models
 #' @export
-results_summary = function(tuner = NULL, num_trials = NULL){
-  if (is.null(tuner)) {
-    warning('Tuner object does not exist')
-  } else {
+results_summary = function(tuner = NULL, num_trials = NULL) {
+  
     tuner$results_summary(num_trials = as.integer(num_trials))
-  }
+  
 }
