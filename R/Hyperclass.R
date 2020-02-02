6fdd83db6f0d14cbd2b742afaad9fab1e461859a
Turgut <turqut.a.314@gmail.com>
2020-02-02 11:11:07 +0400

    new Tuner class and Objective function

--- R/Hyperclass.R
+++ R/Hyperclass.R
@@ -6,3 +6,35 @@ HyperModel_class <- function(){
   object = kerastuner$engine$hypermodel$HyperModel
   return(invisible(object))
 }
+
+#' Tuner class for Keras models.
+#' May be subclassed to create new tuners.
+#' @export
+Tuner_class <- function(){
+  object = kerastuner$Tuner
+  return(invisible(object))
+}
+
+#' @title Objective
+#'
+#' @description Objective(name, direction)
+#'
+#' @details 
+#'
+#' @param name name
+#' @param direction direction
+#' @param ... Some additional arguments
+#'
+#' @export
+Objective <- function(name = NULL, direction = NULL, ...) {
+  
+  args <- c(
+    name = name,
+    direction = direction,
+    ...
+  )
+  do.call(kerastuner$Objective, args)
+}
+
+
+
