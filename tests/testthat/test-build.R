474e09e34997f5e25f29dc20c08dc1c953f81907
Turgut A <turqut.a.314@gmail.com>
2020-02-17 18:46:39 +0400

    Update test-build.R

--- tests/testthat/test-build.R
+++ tests/testthat/test-build.R
@@ -83,6 +83,6 @@ if (!Sys.info()[1] %in% 'Windows') {
   
 }
 
-kerastuneR::plot_tuner(tuner2)
-kerastuneR::plot_tuner(tuner2,height = 500, width = 500)
+p1=kerastuneR::plot_tuner(tuner2)
+p2=kerastuneR::plot_tuner(tuner2,height = 500, width = 500)
 
