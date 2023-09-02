1bb1831e82c91bd51d070599d01918ce479f23e1
turgut090 <turqut.a.314@gmail.com>
2023-09-02 20:32:53 +0400

    fix shape mismatch

--- tests/testthat/test-hyper_class.R
+++ tests/testthat/test-hyper_class.R
@@ -8,10 +8,10 @@ test_succeeds("Can run hyper_class", {
   library(dplyr)
   library(kerastuneR)
   
-  x_data <- matrix(data = runif(500,0,1),nrow = 50,ncol = 5)
+  x_data <- matrix(data = runif(250,0,1),nrow = 50,ncol = 5)
   y_data <- ifelse(runif(50,0,1) > 0.6, 1L,0L) %>% as.matrix()
   
-  x_data2 <- matrix(data = runif(500,0,1),nrow = 50,ncol = 5)
+  x_data2 <- matrix(data = runif(250,0,1),nrow = 50,ncol = 5)
   y_data2 <- ifelse(runif(50,0,1) > 0.6, 1L,0L) %>% as.matrix()
   
   
